9ee0270f7e6f4781cf0873704e08d39306bd3610
0qawa <137458340+0qawa@users.noreply.github.com>
2023-06-23 12:32:35 +0900

    回答回数、正解回数の変数追加
    回答回数、正解回数の取得メソッド、+1するメソッドをそれぞれ作成

--- src/book/QandA.java
+++ src/book/QandA.java
@@ -12,7 +12,8 @@ import java.util.ArrayList;
 
 public class QandA {
     public String question, answer;
-    public Float correctRate;
+    public Float correctRate;   //正答率
+    public int num_res = 0, num_correct = 0;   //回答回数、正解回数
     
     public QandA(String q, String a, float rate){
         question = q;
@@ -32,8 +33,24 @@ public class QandA {
         return correctRate;
     }
     
+    public int getNum_res() {
+        return num_res;
+    }
+
+    public int getNum_correct() {
+        return num_correct;
+    }
+    
     public void SetRate(float rate){
         correctRate = rate;
+    }
+    
+    public void CountNum_res(){
+        num_res++;
+    }
+    
+    public void CountNum_correct(){
+        num_correct++;
     }
     
     public ArrayList<String> ToString(){
